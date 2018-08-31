11eacfe56db3407280eda5dd92f280693b56aad7
mytruongceres <mytruong@ceresolutions.com>
2018-08-31 17:26:58 +0700

    check input

--- index.php
+++ index.php
@@ -29,21 +29,28 @@ while( $result = $a-> fetch_assoc()) {
 	$test = json_encode('a'); 
 
  ?>
+
 <div id="question_<?php echo $result['id'];?>" class='questions' data-question="<?php echo htmlentities($test, ENT_QUOTES, 'UTF-8'); ?>" data-question-id="<?=$result['id']?>" data-answer="<?=$result['answer']?>">
+<div id="quiz">
 <h2 id="question_<?php echo $result['id'];?>"><?php echo $result['id'].".".$result['question_name'];?></h2>
 <div class='align'>
-<input type="radio" value="1" id='radio1_<?php echo $result['id'];?>' name='<?php echo $result['id'];?>'>
-<label id='ans1_<?php echo $result['id'];?>' for='1'><?php echo $result['answer1'];?></label>
+<label id='ans1_<?php echo $result['id'];?>' for='1'>
+<input type="radio" value="1" id='radio1_<?php echo $result['id'];?>' class="input" name='<?php echo $result['id'];?>'>
+	<?php echo $result['answer1'];?></label>
 <br/>
-<input type="radio" value="2" id='radio2_<?php echo $result['id'];?>' name='<?php echo $result['id'];?>'>
-<label id='ans2_<?php echo $result['id'];?>' for='1'><?php echo $result['answer2'];?></label>
+
+<label id='ans2_<?php echo $result['id'];?>' for='1'><input type="radio" value="2" id='radio2_<?php echo $result['id'];?>' class="input" name='<?php echo $result['id'];?>'>
+	<?php echo $result['answer2'];?></label>
 <br/>
-<input type="radio" value="3" id='radio3_<?php echo $result['id'];?>' name='<?php echo $result['id'];?>'>
-<label id='ans3_<?php echo $result['id'];?>' for='1'><?php echo $result['answer3'];?></label>
+
+<label id='ans3_<?php echo $result['id'];?>' for='1'><input type="radio" value="3" id='radio3_<?php echo $result['id'];?>' class="input" name='<?php echo $result['id'];?>'>
+	<?php echo $result['answer3'];?></label>
 <br/>
-<input type="radio" value="4" id='radio4_<?php echo $result['id'];?>' name='<?php echo $result['id'];?>'>
-<label id='ans4_<?php echo $result['id'];?>' for='1'><?php echo $result['answer4'];?></label>
-<input type="radio" checked='checked' value="5" style='display:none' id='radio4_<?php echo $result['id'];?>' name='<?php echo $result['id'];?>'>
+
+<label id='ans4_<?php echo $result['id'];?>' for='1'><input type="radio" value="4" id='radio4_<?php echo $result['id'];?>' class="input" name='<?php echo $result['id'];?>'>
+	<?php echo $result['answer4'];?></label>
+<input type="radio" checked='checked' value="5" style='display:none' id='radio4_<?php echo $result['id'];?>' class="input" name='<?php echo $result['id'];?>'>
+</div>
 </div>
 <br/>
 <input type="button" id='next' value='Next!' name='buttonNext' class='buttNext butt'/>
--- js/jsforex.js
+++ js/jsforex.js
@@ -1,45 +1,61 @@
 $(document).ready(function() {
      var answered = 0;
-     var cNext = 0;
+     var score = 0;
      
    var myQuestion = [];
    myQuestion = ($('form').find(".questions"));
    console.log(myQuestion);
    var count = myQuestion.length;
-  let currentSlide = 0;
+   let currentSlide = 0;
    console.log(count);
+
+      shuffle(myQuestion);
+
+    //ALL FUNCTION
+
+    /*
+    function random question
+     */
+
+    function shuffle(myQuestion) {
+        var m = myQuestion.length,
+            t, i;
+        // continue reverse
+        while (m) {
+            // get a element
+            i = Math.floor(Math.random() * m--);
+            // Then reverse this element
+            t = myQuestion[m];
+            myQuestion[m] = myQuestion[i];
+            myQuestion[i] = t;
+        }
+        return myQuestion;
+    }
+
+
    $('.questions').hide();
   
-  
+  $('.buttNext').on('click', function() {
+       $(myQuestion[currentSlide]).hide();
+        currentSlide++;
+        console.log(currentSlide);
+        showSlide(currentSlide);   
 
-   
-    
+      
+   })
 
-     
-
-
-
-
+  $('.buttPrev').on('click',function(){
+    $(myQuestion[currentSlide]).hide();
+     currentSlide--;
+     showSlide(currentSlide);
+  })
 
        /**
      * This function show the slide in the screen 
      */
     function showSlide(n) {
-       $(myQuestion[currentSlide]).show();
-
-        $('.buttNext').on('click', function() {
-          $(myQuestion[currentSlide]).hide();
-            $(myQuestion[currentSlide]).next().show();
-            currentSlide++;
-            if(curent==count){
-              endQuiz();
-            }
-        
-       })
-    }
-       
-   /*     slides[currentSlide].classList.remove("active-slide");
-        slides[n].classList.add("active-slide");
+     
+        $(myQuestion[currentSlide]).show();
         currentSlide = n;
 
         if (currentSlide === 0) {
@@ -48,18 +64,61 @@ $(document).ready(function() {
             previousButton.style.display = "inline-block";
         }
 
-        if (currentSlide === slides.length - 1) {
-            nextButton.style.display = "none";
+        if (currentSlide === count - 1) {
+            $('.buttNext').hide();
             submitButton.style.display = "inline-block";
+
         } else {
+             $('.buttNext').show();
             nextButton.style.display = "inline-block";
             //submitButton.style.display = "none";
-        }
-    } */
+        }  
+       
+    }
+       
+       function showResults() {
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll(".align");
+        console.log(answerContainers);
+
+        // keep track of user's answers
+        let numCorrect = 0;
+        var questionNumber = 
+
+
+        // for each question...
+        myQuestion.forEach((currentSlide, questionNumber) => {
+            // find selected answer
+            var answerContainer = answerContainers[questionNumber];
+            var selector = `input[id=${questionNumber}]:checked`;
+            var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+            // if answer is correct
+            if (userAnswer === currentSlide.answer) {
+                // add to the number of correct answers
+                numCorrect++;
+                console.log(numCorrect);
+
+                // color the answers green
+                answerContainers[questionNumber].style.color = "lightgreen";
+            } else {
+                // if answer is wrong or blank
+                // color the answers red
+
+                answerContainers[questionNumber].style.color = "red";
+            }
+        });
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestion.length} <br>`;
+        
+    }
 
 //show the next slide
-    function showNextSlide() {
-        showSlide(currentSlide + 1);
+    function checkQuestion() {
+
+
+      
+       
+            
     }
     
 //show the previous slide
@@ -67,7 +126,7 @@ $(document).ready(function() {
         showSlide(currentSlide - 1);
     } 
 
-    var quizContainer = document.getElementById("quiz");
+    var quizContainer =  document.getElementById("quiz");
     var resultsContainer = document.getElementById("results");
     var scoreContainer = document.getElementById("score");
     var submitButton = document.getElementById("submit");
@@ -78,19 +137,28 @@ $(document).ready(function() {
     var previousButton = document.getElementById("prev");
     var nextButton = document.getElementById("next");
 
-    
-    var slides = document.querySelectorAll(".questions");
-    console.log(slides);
-    
-
     showSlide(0);
+
+
+
+
+   $('input[type="radio"]').click(function() {
+        answered++;
+
+
+    }) 
+    
+     $('.buttSub').on('click',function(){
+         if (answered < myQuestion.length) {
+            alert("Bạn đã chọn " + answered + " câu trên " + myQuestion.length + " câu");
+        } 
+        if(currentSlide == count - 1){showResults();}
+    })
   
 
 
 
  
- previousButton.addEventListener("click", showPreviousSlide);
-    nextButton.addEventListener("click", showNextSlide);
 
   })
        
